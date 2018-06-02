fa5b74214a0870656d9ccc83ebb982261f9a92e8
Val Cohen <val@valcohen.com>
2018-06-02 08:45:48 -0700

    Create offset grid of rects

---
+++ Assets/HexGrid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HexGrid : MonoBehaviour {
+
+    public int width  = 6;
+    public int height = 6;
+
+    public HexCell  cellPrefab;
+    public Text     cellLabelPrefab;
+
+    Canvas gridCanvas;
+    HexCell[] cells;
+
+	private void Awake()
+	{
+        gridCanvas = GetComponentInChildren<Canvas>();
+
+        cells = new HexCell[height * width];
+
+        for (int z = 0, i = 0; z < height; z++) {
+            for (int x = 0; x < width; x++) {
+                CreateCell(z, x, i++);
+            }
+        }
+	}
+
+    private void CreateCell(int z, int x, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = x.ToString() + "\n" + z.ToString();
+    }
+}
