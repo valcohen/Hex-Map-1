a64402df917be2146918dc53a917904d7d3a42e3
Val Cohen <val@valcohen.com>
2018-06-03 08:43:51 -0700

    Triangulate blend regions between cells

--- Assets/HexMesh.cs
+++ Assets/HexMesh.cs
@@ -47,29 +47,38 @@ public class HexMesh : MonoBehaviour {
 
     void Triangulate(HexDirection direction, HexCell cell) {
         Vector3 center = cell.transform.localPosition;
-        AddTriangle(
-            center,
-            center + HexMetrics.GetFirstCorner(direction),
-            center + HexMetrics.GetSecondCorner(direction)
-        );
+        Vector3 v1 = center + HexMetrics.GetFirstSolidCorner(direction);
+        Vector3 v2 = center + HexMetrics.GetSecondSolidCorner(direction);
+
+        AddTriangle(center, v1, v2);
+        AddTriangleColor(cell.color);
+
+        Vector3 v3 = center + HexMetrics.GetFirstCorner(direction);
+        Vector3 v4 = center + HexMetrics.GetSecondCorner(direction);
+
+        AddQuad(v1, v2, v3, v4);
+
         HexCell prevNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
         HexCell neighbor     = cell.GetNeighbor(direction)            ?? cell;
         HexCell nextNeighbor = cell.GetNeighbor(direction.Next())     ?? cell;
-        Color edgeColor = (cell.color + neighbor.color) * 0.5f; // avg colors
 
-        AddTriangleColor(
+
+
+        AddQuadColor(
+            cell.color,
             cell.color,
             (cell.color + prevNeighbor.color + neighbor.color) / 3f,
             (cell.color + neighbor.color + nextNeighbor.color) / 3f
         );
     }
 
-
     void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3) {
         int vertexIndex = vertices.Count;
+
         vertices.Add(v1);
         vertices.Add(v2);
         vertices.Add(v3);
+
         triangles.Add(vertexIndex);
         triangles.Add(vertexIndex + 1);
         triangles.Add(vertexIndex + 2);
@@ -86,5 +95,36 @@ public class HexMesh : MonoBehaviour {
         colors.Add(c1);
         colors.Add(c2);
         colors.Add(c3);
+    }
+
+    /*
+     *   v3-------v4
+     *    \       /
+     *     v1---v2
+     *      \   /
+     *       \ /
+     *        v
+     */
+    void AddQuad (Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4) {
+        int vertexIndex = vertices.Count;
+
+        vertices.Add(v1);
+        vertices.Add(v2);
+        vertices.Add(v3);
+        vertices.Add(v4);
+
+        triangles.Add(vertexIndex);
+        triangles.Add(vertexIndex + 2);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 1);
+        triangles.Add(vertexIndex + 2);
+        triangles.Add(vertexIndex + 3);
+    }
+
+    void AddQuadColor (Color c1, Color c2, Color c3, Color c4) {
+        colors.Add(c1);
+        colors.Add(c2);
+        colors.Add(c3);
+        colors.Add(c4);
     }
 }
--- Assets/HexMetrics.cs
+++ Assets/HexMetrics.cs
@@ -3,7 +3,9 @@
 public class HexMetrics {
 
     public const float outerRadius = 10f;
-    public const float innerRadius = outerRadius * 0.866025404f;    // .866 == sqrt(3) / 2
+    public const float innerRadius = outerRadius * 0.866025404f; // .866 == sqrt(3) / 2
+    public const float solidFactor = 0.75f;
+    public const float blendFactor = 1f - solidFactor;
 
     static Vector3[] corners = {
         new Vector3(0f,             0f,  outerRadius),
@@ -21,5 +23,13 @@ public class HexMetrics {
 
     public static Vector3 GetSecondCorner (HexDirection direction) {
         return corners[(int)direction + 1];
+    }
+
+    public static Vector3 GetFirstSolidCorner (HexDirection direction) {
+        return corners[(int)direction] * solidFactor;
+    }
+
+    public static Vector3 GetSecondSolidCorner (HexDirection direction) {
+        return corners[(int)direction + 1] * solidFactor;
     }
 }
