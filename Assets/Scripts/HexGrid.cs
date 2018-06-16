479e728b48b6caf81bb973bca62d81f8af49473b
Val Cohen <val@valcohen.com>
2018-06-16 13:29:53 -0700

    Toggle labels

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -74,6 +74,12 @@ public class HexGrid : MonoBehaviour {
         return cells[x + z * cellCountX];
     }
 
+    public void ShowUI (bool visible) {
+        for (int i = 0; i < chunks.Length; i++) {
+            chunks[i].ShowUI(visible);
+        }
+    }
+
     void CreateCell(int x, int z, int i) {
         Vector3 position;
         position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
--- Assets/Scripts/HexMapEditor.cs
+++ Assets/Scripts/HexMapEditor.cs
@@ -114,4 +114,8 @@ public class HexMapEditor : MonoBehaviour {
     public void SetBrushSize(float size) {
         brushSize = (int)size;
     }
+
+    public void ShowUI (bool visible) {
+        hexGrid.ShowUI(visible);
+    }
 }
