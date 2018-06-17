54057757da374d963f065b8032dd04d6b372cb6e
Val Cohen <val@valcohen.com>
2018-06-17 08:49:37 -0700

    Add edge vertex to support river channels

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -79,7 +79,7 @@ public class HexMesh : MonoBehaviour {
         bridge.y = neighbor.Position.y - cell.Position.y;
         EdgeVertices e2 = new EdgeVertices(
             e1.v1 + bridge,
-            e1.v4 + bridge
+            e1.v5 + bridge
         );
 
         if (cell.GetEdgeType(direction) == HexEdgeType.Slope) {
@@ -102,29 +102,29 @@ public class HexMesh : MonoBehaviour {
          */
         HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
         if (direction <= HexDirection.E && nextNeighbor != null) {
-            Vector3 v5 = e1.v4 + HexMetrics.GetBridge(direction.Next());
+            Vector3 v5 = e1.v5 + HexMetrics.GetBridge(direction.Next());
             v5.y = nextNeighbor.Position.y;
 
             if (cell.Elevation <= neighbor.Elevation) {
                 if (cell.Elevation <= nextNeighbor.Elevation) {
                     TriangulateCorner(
-                        e1.v4, cell, e2.v4, neighbor, v5, nextNeighbor
+                        e1.v5, cell, e2.v5, neighbor, v5, nextNeighbor
                     );
                 } 
                 else { // nxt is lower, rotate cc-wise to keep properly oriented
                     TriangulateCorner(
-                        v5, nextNeighbor, e1.v4, cell, e2.v4, neighbor
+                        v5, nextNeighbor, e1.v5, cell, e2.v5, neighbor
                     );
                 }
             }
             else if (neighbor.Elevation <= nextNeighbor.Elevation) {
                 TriangulateCorner(
-                    e2.v4, neighbor, v5, nextNeighbor, e1.v4, cell
+                    e2.v5, neighbor, v5, nextNeighbor, e1.v5, cell
                 );
             }
             else {
                 TriangulateCorner(
-                    v5, nextNeighbor, e1.v4, cell, e2.v4, neighbor
+                    v5, nextNeighbor, e1.v5, cell, e2.v5, neighbor
                 );
             }
         }
@@ -357,7 +357,9 @@ public class HexMesh : MonoBehaviour {
         AddTriangle(center, edge.v1, edge.v2);
         AddTriangle(center, edge.v2, edge.v3);
         AddTriangle(center, edge.v3, edge.v4);
+        AddTriangle(center, edge.v4, edge.v5);
 
+        AddTriangleColor(color);
         AddTriangleColor(color);
         AddTriangleColor(color);
         AddTriangleColor(color);
@@ -370,7 +372,9 @@ public class HexMesh : MonoBehaviour {
         AddQuad(e1.v1, e1.v2, e2.v1, e2.v2);
         AddQuad(e1.v2, e1.v3, e2.v2, e2.v3);
         AddQuad(e1.v3, e1.v4, e2.v3, e2.v4);
+        AddQuad(e1.v4, e1.v5, e2.v4, e2.v5);
 
+        AddQuadColor(c1, c2);
         AddQuadColor(c1, c2);
         AddQuadColor(c1, c2);
         AddQuadColor(c1, c2);
