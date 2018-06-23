262fb90cf2024e6856327070eefe380799d92c39
Val Cohen <val@valcohen.com>
2018-06-22 19:05:51 -0700

    Tweak river channel depth to avoid cutoffs on tall cliffs

--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -25,13 +25,13 @@ public class HexMetrics {
     public const float horizontalTerraceStepSize = 1f / terraceSteps;
     public const float verticalTerraceStepSize   = 1f / (terracesPerSlope + 1);
 
-    public const float streamBedElevationOffset     = -1f;
+    public const float streamBedElevationOffset     = -1.75f;
     public const float riverSurfaceElevationOffset  = -0.5f;
 
     public static Texture2D noiseSource;
     public const float noiseScale           = 0.003f;
 
-    public const float cellPerturbStrength = 0f; // 4f;
+    public const float cellPerturbStrength = 4f;
     public const float elevationPerturbStrength = 1.5f;
 
     static Vector3[] corners = {
