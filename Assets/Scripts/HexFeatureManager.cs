b4045c4d82345b76609527bdb152aa5dd0e10d45
Val Cohen <val@valcohen.com>
2018-06-30 16:23:00 -0700

    Instantiate & destroy features

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -2,9 +2,27 @@
 
 public class HexFeatureManager : MonoBehaviour {
 
-    public void Clear () {}
+    public Transform featurePrefab;
+
+    Transform container;
+
+    public void Clear () {
+        if (container) {
+            Destroy(container.gameObject);
+        }
+        container = new GameObject("Features Container").transform;
+        container.SetParent(transform, false);
+    }
 
     public void Apply () {}
 
-    public void AddFeature(Vector3 position) {}
+    public void AddFeature(Vector3 position) {
+        Transform instance = Instantiate(featurePrefab);
+
+        // raise the default feature cube so it sits on the ground;
+        // not necessary for meshes whose origin is already at the bottom
+        position.y += instance.localScale.y * 0.5f; 
+        instance.localPosition = HexMetrics.Perturb(position);
+        instance.SetParent(container, false);
+    }
 }
