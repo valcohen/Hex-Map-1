bb13000d1851d53b6046400f942f466ae8c9259b
Val Cohen <val@valcohen.com>
2018-07-08 17:33:01 -0700

    Add walltower prefab & reference

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -6,6 +6,7 @@ public class HexFeatureManager : MonoBehaviour {
         urbanCollections, farmCollections, plantCollections;
 
     public HexMesh walls;
+    public Transform wallTower;
 
     Transform container;
 
