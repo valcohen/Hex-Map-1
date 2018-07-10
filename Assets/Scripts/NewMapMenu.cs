c2fd5dcf0e3cd84fc55fd3a9505a6603f455fa10
Val Cohen <val@valcohen.com>
2018-07-10 15:02:15 -0700

    Wire up create map buttons

--- Assets/Scripts/NewMapMenu.cs
+++ Assets/Scripts/NewMapMenu.cs
@@ -11,4 +11,21 @@ public class NewMapMenu : MonoBehaviour {
     public void Close () {
         gameObject.SetActive(false);
     }
+
+    public void CreateSmallMap () {
+        CreateMap(20, 15);
+    }
+
+    public void CreateMediumMap () {
+        CreateMap(40, 30);
+    }
+
+    public void CreateLargeMap () {
+        CreateMap(80, 60);
+    }
+
+    void CreateMap (int x, int z) {
+        hexGrid.CreateMap(x, z);
+        Close();
+    }
 }
