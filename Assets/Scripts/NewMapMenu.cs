be4f09fd002ab2dbb31342719434c01d6355c1e7
Val Cohen <val@valcohen.com>
2018-07-10 14:53:03 -0700

    Add new map menu UI

---
+++ Assets/Scripts/NewMapMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class NewMapMenu : MonoBehaviour {
+
+    public HexGrid hexGrid;
+
+    public void Open () {
+        gameObject.SetActive(true);
+    }
+
+    public void Close () {
+        gameObject.SetActive(false);
+    }
+}
