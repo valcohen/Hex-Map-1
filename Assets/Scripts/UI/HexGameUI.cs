6bead5bd790b627bf7c545158a068660ce7dbe6a
Val Cohen <val@valcohen.com>
2018-07-28 21:03:05 -0700

    Add game UI, keep track of current cell

---
+++ Assets/Scripts/UI/HexGameUI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HexGameUI : MonoBehaviour {
+    public HexGrid grid;
+
+    public void SetEditMode (bool toggle) {
+        this.enabled = !toggle;
+        grid.ShowUI(!toggle);
+    }
+
+    HexCell currentCell;
+
+    bool UpdateCurrentCell () {
+        HexCell cell = 
+            grid.GetCell(Camera.main.ScreenPointToRay(Input.mousePosition));
+
+        if (cell != currentCell) {
+            currentCell = cell;
+            return true;
+        }
+        return false;
+    }
+
+}
