b6a7e987c4320663772d9c44c9fba68522a92011
Val Cohen <val@valcohen.com>
2018-12-03 18:07:29 -0800

    Manage cell data

---
+++ Assets/Scripts/HexCellShaderData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class HexCellShaderData : MonoBehaviour {
+    Texture2D cellTexture;
+
+    public void Initalize(int x, int z) {
+        if (cellTexture) {
+            cellTexture.Resize(x, z);
+        }
+        else {
+            cellTexture = new Texture2D(
+                x, z, TextureFormat.RGBA32,
+                false   /* mipmaps */,
+                true    /* linear color space */
+            );
+            cellTexture.filterMode = FilterMode.Point;
+            cellTexture.wrapMode   = TextureWrapMode.Clamp;
+        }
+    }
+}
