543adc8f8d92c109d4eed3a6b097fc4fa8c0b52c
Val Cohen <val@valcohen.com>
2018-12-08 12:37:08 -0800

    Add map generator, start in edit mode

---
+++ Assets/Scripts/HexMapGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMapGenerator : MonoBehaviour {
+
+    public HexGrid grid;
+
+    public void GenerateMap (int x, int z) {
+        grid.CreateMap(x, z);
+    }
+}
