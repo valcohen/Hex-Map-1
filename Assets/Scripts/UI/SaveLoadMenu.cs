22e9317f2b5b52bc8418794954f646b92e931590
Val Cohen <val@valcohen.com>
2018-12-09 15:11:51 -0800

    Store negative elevation

--- Assets/Scripts/UI/SaveLoadMenu.cs
+++ Assets/Scripts/UI/SaveLoadMenu.cs
@@ -17,7 +17,7 @@ public class SaveLoadMenu : MonoBehaviour {
 
     bool saveMode;
 
-    const int mapFileVersion = 3;
+    const int mapFileVersion = 4;
 
     public void Open (bool saveMode) {
         this.saveMode = saveMode;
